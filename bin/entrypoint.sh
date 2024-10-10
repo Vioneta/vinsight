48b49e648604783d32948e64346f10924003dca7
nikhuge <nikhuge@gmail.com>
2024-10-10 22:03:44 +0530

    add config file setup for posrtgresql

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-# echo "listen_addresses='*'" >> /etc/postgresql/12/main/postgresql.conf
-# echo "host all  all    127.0.0.1/32  trust" >> /etc/postgresql/12/main/pg_hba.conf
+echo "listen_addresses='*'" >> /etc/postgresql/15/main/postgresql.conf
+echo "host all  all    127.0.0.1/32  trust" >> /etc/postgresql/15/main/pg_hba.conf
 service postgresql start
 service redis-server start
 /usr/local/bin/wait-for-postgres.sh
 # Configure PostgreSQL: Set password and create database if it doesn't exist
-# su - postgres -c 'psql -c "ALTER USER postgres PASSWORD '\''postgres'\'';"'
+su - postgres -c 'psql -c "ALTER USER postgres PASSWORD '\''postgres'\'';"'
+service postgresql restart
 # su - postgres -c "psql -c \"SELECT 'CREATE DATABASE vinsight' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'vinsight')\\gexec\""
 # Start Gunicorn server for Superset
 echo "Starting up Vinsight gunicorn server"
