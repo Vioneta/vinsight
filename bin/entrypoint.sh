31434d62d64ba1129cd83a06978a1fa3d5d0a5f3
nikhuge <nikhuge@gmail.com>
2024-10-10 16:02:58 +0530

    fix pgsql error

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "listen_addresses='*'" >> /etc/postgresql/12/main/postgresql.conf
-echo "host all  all    127.0.0.1/32  trust" >> /etc/postgresql/12/main/pg_hba.conf
+# echo "listen_addresses='*'" >> /etc/postgresql/12/main/postgresql.conf
+# echo "host all  all    127.0.0.1/32  trust" >> /etc/postgresql/12/main/pg_hba.conf
 service postgresql start
 service redis-server start
 /usr/local/bin/wait-for-postgres.sh
