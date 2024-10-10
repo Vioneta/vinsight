c2a843f0a1701a21254bae919a964181e23c2442
nikhuge <nikhuge@gmail.com>
2024-10-10 17:21:54 +0530

    fix pgsql error

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -6,6 +6,8 @@ set -e
 service postgresql start
 service redis-server start
 /usr/local/bin/wait-for-postgres.sh
-su - postgres -c "psql -c \"ALTER USER postgres PASSWORD 'postgres';\""
-# su - postgres -c "psql -c \"CREATE DATABASE vinsight;\""
-gunicorn superset.app:create_app()
+# Configure PostgreSQL: Set password and create database if it doesn't exist
+su - postgres -c 'psql -c "ALTER USER postgres PASSWORD '\''postgres'\'';"'
+su - postgres -c "psql -c \"SELECT 'CREATE DATABASE vinsight' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'vinsight')\\gexec\""
+# Start Gunicorn server for Superset
+exec gunicorn superset.app:create_app()
